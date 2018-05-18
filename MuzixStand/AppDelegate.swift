7948b9516176883353deb8762a66546247d57639
wojowoof <wojowoof@yahoo.com>
2018-05-17 17:15:49 -0700

    How did anything work with this error in here? Sigh ...

--- MuzixStand/AppDelegate.swift
+++ MuzixStand/AppDelegate.swift
@@ -66,11 +66,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewContainerCtrl else { return false }
+//        if topAsDetailController.detailItem == nil {
+//            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+//            return true
+//        }
         return false
     }
     // MARK: - Core Data stack
